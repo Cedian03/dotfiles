fd80eb302ae22a51c4dfdde86e87646bc7a156ff
Cedian03 <nyaalgotjohansson@gmail.com>
2026-05-29 15:28:20 +0200

    add toggle for us/se keyboard layout

--- Configs/hypr/.config/hypr/hyprland.lua
+++ Configs/hypr/.config/hypr/hyprland.lua
@@ -36,6 +36,10 @@ require("modules.workspaces")
 -- })
 
 hl.config({
+    input = {
+        kb_layout = "us,se",
+        kb_options = "grp:win_space_toggle",
+    },
     misc = {
         force_default_wallpaper = 0,     -- Set to 0 or 1 to disable the anime mascot wallpapers
         disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
